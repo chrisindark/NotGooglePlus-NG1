b79451a1527e524a3d2f491f1f85da2c83d2417f
Christopher Paul <christopherp@mindfiresolutions.com>
2017-05-22 21:01:40 +0530

    updated index.js and added Procfile for running a node instance.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 var express = require('express');
-var router = express.Router();
 var path = require('path');
+var router = express.Router();
 
 var app = express();
-app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(path.join(__dirname, 'bower_components')));
-app.use(express.static(path.join(__dirname, 'src')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
+app.use('/', express.static(path.join(__dirname, '.tmp')));
 
-router.get('/*', function (req, res) {
+app.use('', router.get('/*', function (req, res) {
     // load the single view file (angular will handle the page changes on the front-end)
-    res.sendFile('index.html', {root: 'src'});
-});
+    res.sendFile(path.join(__dirname, 'src/index.html'));
+})); 
+
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
