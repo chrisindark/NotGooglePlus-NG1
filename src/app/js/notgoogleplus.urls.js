8e6d8b512af0c193d3fc4c8c7bdb4363cffed255
Christopher Paul <christopherp@mindfiresolutions.com>
2017-07-13 14:16:12 +0530

    refactored files

--- src/app/js/notgoogleplus.urls.js
+++ src/app/js/notgoogleplus.urls.js
@@ -8,8 +8,8 @@
     function ApiUrls() {
         var ApiUrls = {};
         // ApiUrls.domainUrl = 'http://localhost:8000/';
-        ApiUrls.domainUrl = 'http://192.168.1.104:8000/';
-        // ApiUrls.domainUrl = 'https://ancient-tor-16694.herokuapp.com/';
+        // ApiUrls.domainUrl = 'http://192.168.1.104:8000/';
+        ApiUrls.domainUrl = 'https://ancient-tor-16694.herokuapp.com/';
 
         return ApiUrls;
     }
