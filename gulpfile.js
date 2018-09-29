d7adaf46e472d858f18d8b9de24c0f272c0242e7
Christopher Paul <christopher.paul@quikr.com>
2018-09-30 03:51:05 +0530

    kept videojs version as 5 to keep having bower support

--- gulpfile.js
+++
@@ -1,1 +0,0 @@
-'use strict';var gulp = require('gulp');var requireDir  = require( 'require-dir' );var argv = require('yargs').argv;// Require all tasks.requireDir( './gulp-tasks', { recurse: true } );if (argv.production) {    console.log("PRODUCTION ENVIRONMENT");}gulp.task('default', function () {    console.log("GULP IS WORKING");});
